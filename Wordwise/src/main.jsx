4b8025c863ed88f24eacbc2d4ea5a748763429c0
Kishan Kulkarni <98462020+Kishan-Kulkarni@users.noreply.github.com>
2023-06-19 12:21:21 +0530

    route main.jsx

--- Wordwise/src/main.jsx
+++ Wordwise/src/main.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-  
+    <Router>
+    <Routes>
+        <Route path='/*' element={<App />}/>
+    </Routes>
+  </Router>
 )
